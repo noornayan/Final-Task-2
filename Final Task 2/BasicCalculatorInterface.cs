b704616fcffd8bfd538df42120879c913b988791
noornayan <noormdnayan@gmail.com>
2020-11-30 00:24:45 +0600

    final task 2

---
+++ Final Task 2/BasicCalculatorInterface.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Lab_7
+{
+    interface BasicCalculatorInterface
+    {
+        int sum(int x, int y);
+        int sub(int x, int y);
+        int multiplication(int x, int y);
+        int division(int x, int y);
+    }
+    class BasicCalc : BasicCalculatorInterface
+    {
+        int result;
+        public int sum(int number1, int number2)
+        {
+            Console.WriteLine("Sum : " + number1 + " With : " + number2);
+            result = number1 + number2;
+            return result;
+        }
+        public int sub(int number1, int number2)
+        {
+            Console.WriteLine("Sub : " + number1 + " By : " + number2);
+            result = number1 - number2;
+            return result;
+        }
+        public int multiplication(int number1, int number2)
+        {
+            Console.WriteLine("Mul : " + number1 + " With : " + number2);
+            result = number1 * number2;
+            return result;
+        }
+        public int division(int number1, int number2)
+        {
+            Console.WriteLine("Div : " + number1 + " & Divvisor : " + number2);
+            result = number1 / number2;
+            return result;
+        }
+        public void printStates()
+        {
+            Console.WriteLine("Result : " + result);
+        }
+    }
+}
---
+++ Final Task 2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+            Console.WriteLine();
+            Console.WriteLine("Basic Calculator Interface");
+            Console.WriteLine();
+            BasicCalc b = new BasicCalc();
+            Console.WriteLine("Summation Result");
+
+            b.sum(30, 15);
+            b.printStates();
+            Console.WriteLine();
+            Console.WriteLine("Subtraction Result");
+            Console.WriteLine();
+            b.sub(30, 15);
+            b.printStates();
+            Console.WriteLine();
+            Console.WriteLine("Multiplication Result");
+            Console.WriteLine();
+            b.multiplication(30, 15);
+            b.printStates();
+            Console.WriteLine();
+            Console.WriteLine("Division Result");
+             Console.WriteLine();
+            b.division(30, 15);
+            b.printStates();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Scientific Calculator Interface ");
+            Console.WriteLine();
+            SciCal s = new SciCal();
+            Console.WriteLine();
+            Console.WriteLine("Division Result");
+            Console.WriteLine();
+            s.division(21, 3);
+            Console.WriteLine("Factorial Result");
+            Console.WriteLine();
+            s.factorial(5);
+            Console.WriteLine();
+            Console.WriteLine("X to Y Result");
+            Console.WriteLine();
+            s.XtoY(100, 500);
+        }
+    }
+}
